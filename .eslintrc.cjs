14d1302bb95439711778fec9057fc725becd235b
Stepan Shtennikov <shtennikov.s@gmail.com>
2023-11-20 15:28:03 +0500

    init: start project

---
+++ .eslintrc.cjs
@@ -0,0 +1,48 @@
+/* eslint-env node */
+require('@rushstack/eslint-patch/modern-module-resolution');
+
+module.exports = {
+    root: true,
+    extends: [
+        'plugin:vue/vue3-essential',
+        'eslint:recommended',
+        '@vue/eslint-config-typescript',
+        '@vue/eslint-config-prettier/skip-formatting',
+        'eslint-config-airbnb-base',
+        'prettier',
+    ],
+    parserOptions: {
+        ecmaVersion: 'latest',
+    },
+    rules: {
+        'no-console': ['warn', { allow: ['warn', 'error', 'info'] }],
+        'import/prefer-default-export': 'off',
+        'import/no-extraneous-dependencies': 'off',
+        'max-lines-per-function': ['error', 40],
+        'class-methods-use-this': 'off',
+        '@typescript-eslint/lines-between-class-members': 'off',
+        '@typescript-eslint/no-explicit-any': 'error',
+        '@typescript-eslint/no-inferrable-types': 'error',
+        '@typescript-eslint/explicit-function-return-type': 'error',
+        '@typescript-eslint/no-non-null-assertion': 'error',
+        '@typescript-eslint/array-type': [
+            'error',
+            {
+                default: 'array',
+            },
+        ],
+        '@typescript-eslint/explicit-member-accessibility': [
+            'error',
+            {
+                accessibility: 'explicit',
+                overrides: {
+                    accessors: 'explicit',
+                    constructors: 'off',
+                    methods: 'explicit',
+                    properties: 'explicit',
+                    parameterProperties: 'explicit',
+                },
+            },
+        ],
+    },
+};

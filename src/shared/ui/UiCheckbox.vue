c596601056ca482a72ff3af893d3fd30e269da09
Stepan Shtennikov <shtennikov.s@gmail.com>
2023-11-21 15:48:16 +0500

    feat: implement the UiCheckbox component

---
+++ src/shared/ui/UiCheckbox.vue
@@ -0,0 +1,80 @@
+<template>
+    <label class="checkbox">
+        <input v-model="modelValueProxy" v-bind="$attrs" class="checkbox__input" type="checkbox" />
+        <span class="checkbox__box"></span>
+        <slot />
+    </label>
+</template>
+
+<script setup lang="ts">
+import { useModelProxy } from '@shared/lib/composables/useModelProxy';
+
+defineProps({
+    modelValue: {
+        type: [Boolean, Array, Set],
+        required: true,
+    },
+});
+
+defineEmits(['update:modelValue']);
+
+defineOptions({
+    inheritAttrs: false,
+});
+
+const modelValueProxy = useModelProxy();
+</script>
+
+<style scoped>
+.checkbox {
+    display: inline-block;
+    position: relative;
+    padding-left: 44px;
+    margin-bottom: 10px;
+    text-align: left;
+    cursor: pointer;
+    font-size: 20px;
+    line-height: 28px;
+    transition-duration: 0.3s;
+    transition-timing-function: ease;
+    transition-property: background-color, border-color;
+}
+
+.checkbox > input.checkbox__input {
+    position: absolute;
+    z-index: -1;
+    opacity: 0;
+    padding: 0;
+}
+
+.checkbox > .checkbox__box {
+    border: 2px solid var(--blue-light);
+}
+
+.checkbox > .checkbox__box {
+    border-radius: 8px;
+    background: 0 0;
+    position: absolute;
+    top: 0;
+    left: 0;
+    height: 28px;
+    width: 28px;
+}
+
+.checkbox > input.checkbox__input:checked ~ .checkbox__box:after {
+    display: block;
+}
+
+.checkbox > .checkbox__box:after {
+    content: '';
+    position: absolute;
+    display: none;
+    top: 50%;
+    left: 50%;
+    width: 14px;
+    height: 13px;
+    background-image: url('@shared/assets/icons/icon-check.svg');
+    border: none;
+    transform: translate(-50%, -50%);
+}
+</style>
--- src/shared/ui/index.ts
+++ src/shared/ui/index.ts
@@ -1,3 +1,4 @@
 import UiButtonVue from '@shared/ui/UiButton.vue';
+import UiCheckbox from '@shared/ui/UiCheckbox.vue';
 
-export { UiButtonVue };
+export { UiButtonVue, UiCheckbox };

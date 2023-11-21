f3b8d98dc6ca2c93e7b4087a043d74ccbc89c4e3
Stepan Shtennikov <shtennikov.s@gmail.com>
2023-11-21 16:45:51 +0500

    feat: implement the UiCard component

--- src/shared/ui/index.ts
+++ src/shared/ui/index.ts
@@ -1,5 +1,6 @@
 import UiButtonVue from '@shared/ui/UiButton.vue';
 import UiCheckbox from '@shared/ui/UiCheckbox.vue';
 import UiIconVue from '@shared/ui/UiIcon.vue';
+import UiCard from '@shared/ui/UiCard.vue';
 
-export { UiButtonVue, UiCheckbox, UiIconVue };
+export { UiButtonVue, UiCheckbox, UiIconVue, UiCard };

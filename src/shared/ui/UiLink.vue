676d82851e81ee1d071db64fbe2ffdcee1dd1d3a
Stepan Shtennikov <shtennikov.s@gmail.com>
2023-11-22 14:07:44 +0500

    feat: implement the UiLink component

---
+++ src/shared/ui/UiLink.vue
@@ -0,0 +1,20 @@
+<template>
+    <component :is="tag" class="link">
+        <slot />
+    </component>
+</template>
+
+<script setup lang="ts">
+withDefaults(defineProps<{ tag?: string | object }>(), { tag: 'RouterLink' });
+</script>
+
+<style scoped>
+.link {
+    color: var(--blue);
+    text-decoration: none;
+}
+
+.link:hover {
+    text-decoration: underline;
+}
+</style>
--- src/shared/ui/index.ts
+++ src/shared/ui/index.ts
@@ -4,5 +4,6 @@ import UiIconVue from '@shared/ui/UiIcon.vue';
 import UiCard from '@shared/ui/UiCard.vue';
 import UiTab from '@shared/ui/UiTab.vue';
 import UiTabs from '@shared/ui/UiTabs.vue';
+import UiLink from '@shared/ui/UiLink.vue';
 
-export { UiButtonVue, UiCheckbox, UiIconVue, UiCard, UiTab, UiTabs };
+export { UiButtonVue, UiCheckbox, UiIconVue, UiCard, UiTab, UiTabs, UiLink };

0a95f593672906fb279038402ce8c3926559e20e
Stepan Shtennikov <shtennikov.s@gmail.com>
2023-11-22 13:06:20 +0500

    build: add vueJsx plugin for vite

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,11 @@ import { fileURLToPath, URL } from 'node:url';
 
 import { defineConfig, splitVendorChunkPlugin } from 'vite';
 import vue from '@vitejs/plugin-vue';
+import vueJsx from '@vitejs/plugin-vue-jsx';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [vue(), splitVendorChunkPlugin()],
+    plugins: [vue(), splitVendorChunkPlugin(), vueJsx()],
     resolve: {
         alias: {
             '@': fileURLToPath(new URL('./src', import.meta.url)),

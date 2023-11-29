4717a644f4020db43296761d06dd9e43e31c4839
Stepan Shtennikov <shtennikov.s@gmail.com>
2023-11-29 15:46:52 +0500

    fix: fix a problem with losing reactivity in useModelProxy composable

--- src/shared/lib/composables/useModelProxy.ts
+++ src/shared/lib/composables/useModelProxy.ts
@@ -2,7 +2,13 @@ import { computed, getCurrentInstance, toRef } from 'vue';
 
 export function useModelProxy<T>() {
     const instance = getCurrentInstance();
-    const modelValue = toRef(instance?.props.modelValue);
+    const props = instance?.props;
+    if (!props) {
+        throw new Error(
+            'A component that uses useModelProxy must have a props with the modelValue property',
+        );
+    }
+    const modelValue = toRef(instance?.props, 'modelValue');
     const emit = instance?.emit;
 
     return computed<T>({

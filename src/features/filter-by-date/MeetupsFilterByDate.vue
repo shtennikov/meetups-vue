a264b774a852a01c799e055c484fa5b0c1048606
Stepan Shtennikov <shtennikov.s@gmail.com>
2023-11-29 14:47:08 +0500

    feat: implement the MeetupsFilterByDate component

---
+++ src/features/filter-by-date/MeetupsFilterByDate.vue
@@ -0,0 +1,25 @@
+<template>
+    <UiRadioGroup v-model="modelProxy" :options="dateFilterOptions" name="date" />
+</template>
+
+<script setup lang="ts">
+import { UiRadioGroup } from '@shared/ui';
+import { useModelProxy } from '@shared/lib/composables';
+
+const props = defineProps<{
+    modelValue: string;
+}>();
+defineEmits<{
+    'update:modelValue': [value: string];
+}>();
+
+const modelProxy = useModelProxy<typeof props.modelValue>();
+
+const dateFilterOptions = [
+    { text: 'Все', value: 'all' },
+    { text: 'Прошедшие', value: 'past' },
+    { text: 'Ожидаемые', value: 'future' },
+];
+</script>
+
+<style scoped></style>
---
+++ src/features/filter-by-date/index.ts
@@ -0,0 +1,3 @@
+import MeetupsFilterByDate from '@features/filter-by-date/MeetupsFilterByDate.vue';
+
+export { MeetupsFilterByDate };

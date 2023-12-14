f9d63df690bd424ffd4509a1ae2c9b6746ad2117
Stepan Shtennikov <shtennikov.s@gmail.com>
2023-12-14 15:43:37 +0500

    feat: implement meetups page

--- src/pages/meetups/PageMeetups.vue
+++ src/pages/meetups/PageMeetups.vue
@@ -1,9 +1,30 @@
 <template>
-    <div>
-        <div>Meetups</div>
-    </div>
+    <UiContainer>
+        <!-- TODO filter data emit -->
+        <MeetupsFilters />
+        <template v-if="meetups">
+            <!-- TODO add calendar view -->
+            <MeetupsList :meetups="meetups" />
+        </template>
+        <UiAlert v-else>{{ ALERT_TEXT }}</UiAlert>
+    </UiContainer>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from 'vue';
+import type { Meetup } from '@shared/types';
+import { meetupsRepository } from '@shared/api';
+import { UiContainer, UiAlert } from '@shared/ui';
+import { MeetupsFilters } from '@widgets/meetups-filters';
+import MeetupsList from './ui/MeetupsList.vue';
+
+const ALERT_TEXT = 'Загрузка...';
+// TODO add filteredMeetups
+const meetups = ref<Meetup[] | null>(null);
+
+meetupsRepository.getMeetups().then((data) => {
+    meetups.value = data;
+});
+</script>
 
 <style scoped></style>
---
+++ src/pages/meetups/ui/MeetupsList.vue
@@ -0,0 +1,40 @@
+<template>
+    <UiTransitionGroupFade class="meetups-list">
+        <li v-for="meetup in meetups" :key="meetup.id" class="meetups-list__item">
+            <!-- TODO Meetup Page link -->
+            <!-- <RouterLink
+                :to="{ name: 'meetup', params: { meetupId: meetup.id } }"
+                class="meetups-list__item-link"
+                tabindex="0"
+            > -->
+            <MeetupCard :meetup="meetup" />
+            <!-- </RouterLink> -->
+        </li>
+    </UiTransitionGroupFade>
+</template>
+
+<script setup lang="ts">
+import { MeetupCard } from '@entities/meetup-card';
+import { UiTransitionGroupFade } from '@shared/ui';
+import type { Meetup } from '@shared/types';
+
+defineProps<{ meetups: Meetup[] }>();
+</script>
+
+<style scoped>
+.meetups-list {
+    margin: 0;
+    padding: 0;
+    list-style: none;
+}
+
+.meetups-list__item {
+    margin: 0 0 32px 0;
+    text-decoration: none;
+}
+
+.meetups-list__item-link {
+    text-decoration: none;
+    color: inherit;
+}
+</style>

2ae2fbcdc5a96f0c7943701142f8cdb534f51ccd
Stepan Shtennikov <shtennikov.s@gmail.com>
2023-12-14 11:12:44 +0500

    chore: add types for meetups

--- src/shared/types/index.ts
+++ src/shared/types/index.ts
@@ -9,3 +9,26 @@ export interface DayData {
     stringDate: string;
     isCurrentMonth: boolean;
 }
+
+export interface Agenda {
+    id: number;
+    startsAt: string;
+    endsAt: string;
+    type: string;
+    title: string | null;
+    description: string | null;
+    speaker: string | null;
+    language: string | null;
+}
+export interface Meetup {
+    id: number;
+    title: string;
+    description: string;
+    imageId: number;
+    image: string;
+    date: number;
+    dateIso: string;
+    organizer: string;
+    place: string;
+    agenda: Agenda[];
+}

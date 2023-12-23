dede051a5f1bc6fc56a7bcd5605b1b8090cfd27d
Stepan Shtennikov <shtennikov.s@gmail.com>
2023-12-23 17:28:07 +0500

    fix: add an optional modifier to the UiAlert text prop

--- src/shared/ui/UiAlert.vue
+++ src/shared/ui/UiAlert.vue
@@ -5,7 +5,7 @@
 </template>
 
 <script setup lang="ts">
-withDefaults(defineProps<{ text: string }>(), { text: 'Error...' });
+withDefaults(defineProps<{ text?: string }>(), { text: 'Error...' });
 </script>
 
 <style scoped>
